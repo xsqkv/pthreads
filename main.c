d932eb89b0421c0692148358f6c8276d3a84ce1e
Artem Vanty <artem.vanty.0358@gmail.com>
2023-10-21 17:35:32 +0900

    Add

---
+++ main.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+typedef unsigned char ubyte;
+
+unsigned long long a[8],b[8];
+
+void *f(void *n)
+{
+	for(;;)
+	{
+		++a[(ubyte)n];
+	}	
+	pthread_exit(0);
+}
+
+typedef struct thread
+{
+	void *data;
+	pthread_t thr;
+} thread;
+
+int main(void)
+{
+	ubyte N = 2;
+	pthread_t threads[N];
+	ubyte i = 0;
+	for(;i^N;++i)
+	{
+		//threads[i].data = (int *)(i+1);
+		pthread_create(&threads[i],NULL,f,(ubyte)i);
+		//pthread_join(threads[i].thr,NULL);
+	}
+
+	unsigned long long sumA = 0,sumB=0;
+	i=0;
+	for(;;)
+	{
+		for(i=0;i^N;++i)
+		{
+			sumA += a[i];
+			sumB += b[i];
+		}
+		printf("\ec");
+		printf("%lluM\n", (sumA - sumB)/1000000);
+		sumB = sumA;
+		for(i=0;i^N;++i)
+		{
+			b[i] = a[i];
+		}
+		sleep(1);
+	}
+
+	return 0;
+}
